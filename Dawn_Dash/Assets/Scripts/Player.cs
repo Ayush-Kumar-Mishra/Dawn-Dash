770e1c2f1741decc7a121a2711de5f2ab40c258b
Ayush Kumar Mishra <ayushmishra1376@gmail.com>
2023-12-15 11:31:33 +0530

    Updates

--- Dawn_Dash/Assets/Scripts/Player.cs
+++ Dawn_Dash/Assets/Scripts/Player.cs
@@ -8,10 +8,26 @@ public class Player : MonoBehaviour
     public float speed_F;
     public float speed_LR;
     float xInput;
+    float timer;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
 
     void Update()
     {
+        timer = Time.deltaTime;
         xInput = Input.GetAxis("Horizontal");
         transform.position = new Vector3(transform.position.x + xInput*speed_LR, transform.position.y, transform.position.z + speed_F);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacles")
+        {
+            Time.timeScale = 0;
+            Destroy(this.gameObject, 1);
+        }
     }
 }
